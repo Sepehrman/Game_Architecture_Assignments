85e50825842bf1d21eef6e4d09874ee121ff2465
Jeff Phan <jeffphan99@gmail.com>
2024-03-10 16:17:42 -0700

    seperated maze and arkanoids to seperate folders, added lab10

---
+++ Arkanoids/Assignment3/Assignment3/Assignment3App.swift
@@ -0,0 +1,17 @@
+//
+//  Assignment3App.swift
+//  Assignment3
+//
+//  Created by Jeff Phan on 2024-03-10.
+//
+
+import SwiftUI
+
+@main
+struct Assignment3App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
